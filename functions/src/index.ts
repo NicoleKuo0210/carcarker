424df9432886b7f8c2619d31437f37be7e74fe80
NicoleKuo0210 <yuchunkuo210@gmail.com>
2025-05-29 12:33:55 +0800

    Functions/Storage Data transmit / a little UI

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,42 +1,49 @@
-
-import functions from 'firebase-functions';
+import * as functions from 'firebase-functions';
 import admin from 'firebase-admin';
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 
 admin.initializeApp();
 
 const db = admin.firestore();
 const storage = admin.storage().bucket();
-const Busboy = require('busboy');
 const app = express();
-app.use(cors({ origin: true }));
-app.use(express.json({ limit: '10mb' })); // Allow large image payloads
+const Busboy = require('busboy');
 
-app.post('/upload', (req, res) => {
+app.use(cors({ origin: true }));
+app.use(express.json({ limit: '10mb' }));
+
+app.post('/upload', (req: Request, res: Response) => {
   const busboy = new Busboy({ headers: req.headers });
   let uploadData: Buffer = Buffer.alloc(0);
   let metadata: any = {};
 
-  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-    file.on('data', (data) => {
-      uploadData = Buffer.concat([uploadData, data]);
-    });
+  busboy.on(
+    'file',
+    (
+      fieldname: string,
+      file: NodeJS.ReadableStream,
+      filename: string,
+      encoding: string,
+      mimetype: string
+    ) => {
+      file.on('data', (data: Buffer) => {
+        uploadData = Buffer.concat([uploadData, data]);
+      });
 
-    file.on('end', () => {
-      metadata.filename = filename;
-      metadata.mimetype = mimetype;
-    });
-  });
+      file.on('end', () => {
+        metadata.filename = filename;
+        metadata.mimetype = mimetype;
+      });
+    }
+  );
 
-  busboy.on('field', (fieldname, val) => {
+  busboy.on('field', (fieldname: string, val: string) => {
     metadata[fieldname] = val;
   });
 
   busboy.on('finish', async () => {
-    const timestamp = Date.now();
-    const filename = `esp32_images/${timestamp}_${metadata.filename}`;
-
+    const filename = `esp32_images/latest.jpg`;
     const file = storage.file(filename);
     await file.save(uploadData, {
       metadata: { contentType: metadata.mimetype },
@@ -44,12 +51,13 @@ app.post('/upload', (req, res) => {
     });
 
     const imageUrl = `https://storage.googleapis.com/${storage.name}/${filename}`;
-    await db.collection('esp32_logs').add({
+    await db.collection('esp32_logs').doc('latest').set({
       temperature: metadata.temperature,
       humidity: metadata.humidity,
       imageUrl,
       timestamp: admin.firestore.FieldValue.serverTimestamp(),
     });
+
 
     res.status(200).json({ success: true, imageUrl });
   });
@@ -57,27 +65,4 @@ app.post('/upload', (req, res) => {
   req.pipe(busboy);
 });
 
-
-// Export function
 export const api = functions.https.onRequest(app);
-
-
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-// import {onRequest} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
-
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
